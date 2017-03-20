8160b3ec3b724cb8b2d336006b1d0505b748f1ee
lanmingxiang <lmxdawn@qq.com>
2017-03-20 18:32:07 +0800

    修改网站名称

--- application/extra/sys_config.php
+++ application/extra/sys_config.php
@@ -16,5 +16,5 @@ return [
     ,'lmx_upload_url'        =>      "http://localhost/think-admin/public/uploads/" //上传文件的根地址
     ,'lmx_static_debug'       =>      false //静态文件的调试模式
     ,'lmx_version'          =>       '2017010853' // 版本号
-    ,'lmx_title'            =>      '双开后台管理系统' //网站名称
+    ,'lmx_title'            =>      '后台管理系统' //网站名称
 ];
