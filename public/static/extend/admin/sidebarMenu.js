5f67cf5d33de152c5bbffab8d623d13d1581a659
lanmingxiang <lmxdawn@qq.com>
2017-04-20 14:32:24 +0800

    完善手风琴菜单

--- public/static/extend/admin/sidebarMenu.js
+++ public/static/extend/admin/sidebarMenu.js
@@ -6,41 +6,26 @@
 layui.define(function(exports){ //提示：组件也可以依赖其它组件，如：layui.define('layer', callback);
     var obj = {
         show: function(menu){
-            var animationSpeed = 300;
-            $(menu).on('click', 'li a', function(e) {
-                var $this = $(this);
-                var checkElement = $this.next();
+            var animationSpeed = 150;
 
-                if (checkElement.is('.treeview-menu') && checkElement.is(':visible')) {
-                    checkElement.slideUp(animationSpeed, function() {
-                        checkElement.removeClass('menu-open');
-                    });
-                    checkElement.parent("li").removeClass("active");
+            $(menu).on( "click",function(event) {
+                var closest_a = $(event.target).closest("a");
+                if (!closest_a || closest_a.length == 0) {
+                    return
                 }
-
-                //If the menu is not visible
-                else if ((checkElement.is('.treeview-menu')) && (!checkElement.is(':visible'))) {
-                    //Get the parent menu
-                    var parent = $this.parent('ul').first();
-                    //Close all open menus within the parent
-                    //var ul = parent.find('ul:visible').slideUp(animationSpeed);
-                    //Remove the menu-open class from the parent
-                    //ul.removeClass('menu-open');
-                    //Get the parent li
-                    var parent_li = $this.parent("li");
-                    //Open the target menu and add the menu-open class
-                    checkElement.slideDown(animationSpeed, function() {
-                        //Add the class active to the parent li
-                        checkElement.addClass('menu-open');
-                        parent.find('li.active').removeClass('active');
-                        parent_li.addClass('active');
+                var closest_a_next = closest_a.next().get(0);
+                if (!$(closest_a_next).is(":visible")) {
+                    var closest_ul = $(closest_a_next.parentNode).closest("ul");
+                    closest_ul.find("> .treeview > .treeview-menu").each(function() {
+                        if (this != closest_a_next && !$(this.parentNode).hasClass("active")) {
+                            $(this).slideUp(animationSpeed).parent().removeClass("treeview")
+                        }
                     });
                 }
-                //if this isn't a link, prevent the page from being redirected
-                if (checkElement.is('.treeview-menu')) {
-                    e.preventDefault();
-                }
+                $(closest_a_next).slideToggle(animationSpeed).parent().addClass("treeview");
+                return false;
             });
+
         }
     };
 
